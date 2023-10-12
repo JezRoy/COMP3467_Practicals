a3ff0e4d363590408bf924301d441cd8c73a594a
Jeremy Roy <jezoffy@gmail.com>
2023-10-12 14:58:38 +0100

    bug fixes

--- Practical2/task1.c
+++ Practical2/task1.c
@@ -50,9 +50,9 @@ int main(int argc, char** argv) {
         // Create a buffer to help receive the right amount of data from the incoming above based on the above.
         int* msgLen = (int*)malloc(sizeof(int) * count);
 
-        MPI_Recv(&message, msgLen, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&message, &msgLen, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
         printf("Rank 1 received (from rank 0): %s",message);
-        printf("Message length was: %d", msgLen);
+        printf("Message length was: %d", &msgLen);
 
         free(msgLen);
     }
