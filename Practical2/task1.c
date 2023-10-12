78bcab91757eb6795bb5cd701e8659995969f83f
Jeremy Roy <jezoffy@gmail.com>
2023-10-12 09:49:17 +0100

    adding sending and recieving

--- Practical2/task1.c
+++ Practical2/task1.c
@@ -35,7 +35,6 @@ int main(int argc, char** argv) {
     } else if (rank == 1) {
         // Rank no.1
         MPI_Recv(&message, 14, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf()
         printf("The message is: %s",message);
     }
 
